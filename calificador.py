98b5330e66f7e1f876bb5faebccdf9ce008b951e
JhanAntezana <jhan.antezana6174@gmail.com>
2023-05-14 22:45:04 -0500

    correccion de guardado

--- calificador.py
+++ calificador.py
@@ -13,24 +13,22 @@ rutas_archivos = []
 alumnos = []
 ruta_base = "./evaluaciones"
 
-def obtener_datos_alumnos(ruta_base: str):
+def obtener_datos_alumnos(ruta_base: str): #obtener todos los datos del alumno
+                                            #nombre de los alumno
+                                            #evaluaciones que rindio (PC1, PC2 ....)
     global alumnos
     archivos = os.listdir(ruta_base)
     alumnos = [nombre for nombre in archivos if os.path.isdir(os.path.join(ruta_base, nombre))]
     #for nombre in archivos:
        # if os.path.isdir(os.path.join(ruta_base, nombre)):
         #    alumnos.append(nombre)
+
+def obtener_datos_evaluacion():#pregustas que realizo
+                                #nombre del archivo por pregunta
+                                
+    pass
 obtener_datos_alumnos(ruta_base)
 print("Alumnos: ", alumnos)
-
-def guardarNombresArchivo(ruta_carpeta):
-    for item in os.listdir(ruta_carpeta):
-        ruta_completa = os.path.join(ruta_carpeta, item)
-        if os.path.isfile(ruta_completa):
-            archivos.append(item)
-            rutas_archivos.append(ruta_completa)
-
-
 
 
  
